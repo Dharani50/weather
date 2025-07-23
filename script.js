92322087747d7e0ff70b46497e5fe3ff242c95b6
Dharani50 <146551565+Dharani50@users.noreply.github.com>
2025-07-23 04:43:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const apiKey = 'd5846d018de3fd72b10cb81648daa551'; // Replace with your OpenWeatherMap API key
+
+function getWeather() {
+  const city = document.getElementById('city-input').value;
+ const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+ // °F
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('city-name').textContent = data.name + ', ' + data.sys.country;
+      document.getElementById('weather-type').textContent = data.weather[0].description;
+      document.getElementById('temp').textContent = data.main.temp;
+      document.getElementById('min-temp').textContent = data.main.temp_min;
+      document.getElementById('max-temp').textContent = data.main.temp_max;
+    })
+    .catch(error => {
+      alert("City not found or API error");
+      console.error(error);
+    });
+}
